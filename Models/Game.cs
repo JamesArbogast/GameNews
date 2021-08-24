b7747bdd09cd7630aa89bb1819b3f5355879ffa1
James Arbogast <jamesarbogast52@gmail.com>
2021-08-24 14:25:49 -0700

    init commit

---
+++ Models/Game.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameStock.Models
+{
+    public class Game
+    {
+        [Key]
+        public int GameId { get; set; }
+
+        [Required(ErrorMessage = "Is required.")]
+        [MinLength(2, ErrorMessage = "Must be at least 2 characters.")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        public List<string> Genres { get; set; }
+        public List<string> Platforms { get; set; }
+        public List<int> Prices { get; set; }
+        public List<string> Vendors { get; set; }
+        public int CurrentRating { get; set; } 
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+    }
+}
