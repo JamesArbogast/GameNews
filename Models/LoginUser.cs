b3711bcf42da4fb39fb076edc1877f1f1e050a22
SimplyyJoji <59666562+SimplyyJoji@users.noreply.github.com>
2021-08-23 17:06:45 -0700

    commit message

---
+++ Models/LoginUser.cs
@@ -0,0 +1,20 @@
+namespace GameNews.Models
+{
+    public class LoginUser
+    {
+         [NotMapped] // don't add table to db
+    public class LoginUser
+    {
+        [Required(ErrorMessage = "is required.")]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string LoginEmail { get; set; }
+
+        [Required(ErrorMessage = "is required.")]
+        [MinLength(8, ErrorMessage = "must be at least 8 characters")]
+        [DataType(DataType.Password)] // auto fills input type attr
+        [Display(Name = "Password")]
+        public string LoginPassword { get; set; }
+    }
+}
+}
