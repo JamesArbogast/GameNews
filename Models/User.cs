a7067a86c5c37ebc317278ce6b12db3d8dc4f70d
Lawrence <45595421+Zirkander@users.noreply.github.com>
2021-08-23 20:37:12 -0700

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,40 +1,44 @@
-using System.ComponentModel.DataAnnotations;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace ProjName.Models
-  {
-      public class User
-      {
-        [Key] // the below prop is the primary key, [Key] is not needed if named with pattern: ModelNameId
+namespace GameNews.Models
+{
+    public class User
+    {
+        [Key]
         public int UserId { get; set; }
 
-        [Required(ErrorMessage = "is required")]
-        [MinLength(2, ErrorMessage = "must be at least 2 characters")]
-        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "Is required.")]
+        [MinLength(2, ErrorMessage = "Must be at least 2 characters.")]
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "is required.")]
-        [MinLength(2, ErrorMessage = "must be at least 2 characters")]
+        [Required(ErrorMessage = "Is requried.")]
+        [MinLength(2, ErrorMessage = "Must be at least 2 characters.")]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "is required.")]
+        [Required(ErrorMessage = "Is required.")]
         [EmailAddress]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "is required.")]
-        [MinLength(8, ErrorMessage = "must be at least 8 characters")]
-        [DataType(DataType.Password)] // auto fills input type attr
+        [Required(ErrorMessage = "Is requried.")]
+        [MinLength(8, ErrorMessage = "Must be at least 8 characters.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$", ErrorMessage = "Password is Invalid, Please make sure that there is at least 1 uppercase, 1 lowercase, and 1 special character.")]
+        [Display(Name = "Password")]
         public string Password { get; set; }
 
-        [NotMapped] // don't add to DB
+        [NotMapped] //Makes sure that it doesn't get added to DB
+        [Required(ErrorMessage = "Is requried.")]
         [DataType(DataType.Password)]
-        [Compare("Password", ErrorMessage = "doesn't match password")]
+        [Compare("Password", ErrorMessage = "Passwords don't match!")]
         [Display(Name = "Confirm Password")]
-        public string PasswordConfirm { get; set; }
+        public string ConfirmPassword { get; set; }
 
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
-      }
-  }
+
+    }
+}
