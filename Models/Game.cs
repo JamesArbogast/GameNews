b33e1f7d6413c937d581339d0e8a3742cad9cb24
James Arbogast <jamesarbogast52@gmail.com>
2021-08-25 14:34:07 -0700

    lawrence RestSharp update

--- Models/Game.cs
+++ Models/Game.cs
@@ -5,30 +5,30 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GameStock.Models
 {
-	public class Game
-	{
-		[Key]
-		public int GameId { get; set; }
+    public class Game
+    {
+        [Key]
+        public int GameId { get; set; }
 
-		[Required(ErrorMessage = "Is required.")]
-		[MinLength(2, ErrorMessage = "Must be at least 2 characters.")]
-		[Display(Name = "Name")]
-		public string Name { get; set; }
-		public string ImgUrl { get; set; }
+        [Required(ErrorMessage = "Is required.")]
+        [MinLength(2, ErrorMessage = "Must be at least 2 characters.")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        public string ImgUrl { get; set; }
 
-		public List<string> Genres { get; set; }
-		public List<string> Platforms { get; set; }
-		public List<int> Prices { get; set; }
-		public List<string> Vendors { get; set; }
-		public int CurrentRating { get; set; } 
-		public string Creator { get; set; }
+        public string Genres { get; set; }
+        public string Platforms { get; set; }
+        public int Prices { get; set; }
+        public string Vendors { get; set; }
+        public int CurrentRating { get; set; }
+        public string Creator { get; set; }
 
-		public DateTime CreatedAt { get; set; } = DateTime.Now;
-		public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
 
-		public int UserId { get; set; }
-		public User CreatedBy { get; set; }
+        public int UserId { get; set; }
+        public User CreatedBy { get; set; }
 
-		public List<LikedGames> LikedGames { get; set; }
-	}
+        public List<LikedGames> LikedGames { get; set; }
+    }
 }
