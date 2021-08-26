651d6ca0925d4ba3944424a5d1435cc057336662
James Arbogast <jamesarbogast52@gmail.com>
2021-08-26 16:06:24 -0700

    updated dashboard

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -79,6 +79,10 @@ namespace GameStock.Controllers
             List<Game> allGames = db.Games // hover over the param to see it's data type
                                            // .Include(game => game.LikedGames)
                 .ToList();
+
+            
+            ViewBag.reviews = db.GameReviews;
+            
             return View("Dashboard", allGames);
         }
         //READ
